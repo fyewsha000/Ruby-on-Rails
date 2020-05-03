a6c4266effbb136113734eff74f216e5c97c31d0
佐々木 優葉 <yuha000@MacBook-Pro.local>
2020-05-03 15:10:17 +0900

    [finish]4-8

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,18 @@
 class BlogsController < ApplicationController
 
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
+  def destroy
+  	blog = Blog.find(params[:id])
+  	blog.destroy
+  	redirect_to blogs_path
+  end
+
   def show
   	@blog = Blog.find(params[:id])
   end
@@ -22,7 +35,10 @@ class BlogsController < ApplicationController
   	@blog = Blog.find(params[:id])
   end
 
+
+
   private
+
   def blog_params
   	params.require(:blog).permit(:title,:category,:body)
   end
